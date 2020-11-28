9e5b208c6eb128602a8d836b322b98e915fcb20b
hyojin530 <dkakfl56@gmail.com>
2020-11-28 10:44:30 +0900

    cart ajax check

--- product/product.py
+++ product/product.py
@@ -236,7 +236,7 @@ def check_payment():
     html = render_template('paycomplete.html', data=post_detail)
     return html
 
-@product_dao.route('/bakctomain')
+@product_blue.route('/bakctomain')
 def backtomain():
     html = render_template('backtomain.html')
     return html
